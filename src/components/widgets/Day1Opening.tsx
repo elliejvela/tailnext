d6db65a1a23b61c128e9f97e91b8c9ac26a2845d
elliejvela <ellie.j.vela@stanford.edu>
2026-09-15 23:08:43 -0700

    Schedule update 1 - day 1 opening plenary

--- src/components/widgets/Day1Opening.tsx
+++ src/components/widgets/Day1Opening.tsx
@@ -10,16 +10,19 @@ export default function Day1Opening(){
               <tr>
                 <th className="px-4 py-3 text-left font-semibold w-48">Time</th>
                 <th className="px-4 py-3 text-left font-semibold">Session</th>
+                <th className="px-4 py-3 text-left font-semibold">Location</th>
               </tr>
             </thead>
             <tbody className="divide-y divide-gray-100">
               <tr>
                 <td className="px-4 py-3 whitespace-nowrap">8:30 AM – 9:30 AM</td>
                 <td className="px-4 py-3">Registration, Check-In &amp; Breakfast</td>
+                <td className="px-4 py-3">Blackburn Ballroom</td>
               </tr>
               <tr className="bg-gray-50">
                 <td className="px-4 py-3 whitespace-nowrap">9:30 AM – 9:35 AM</td>
                 <td className="px-4 py-3"><b>Welcome:</b> Wayne A.I. Frederick, Interim President, Howard University</td>
+                <td className="px-4 py-3">Blackburn Ballroom</td>
               </tr>
               <tr>
                 <td className="px-4 py-3 whitespace-nowrap">9:35 AM – 9:40 AM</td>
@@ -27,23 +30,30 @@ export default function Day1Opening(){
                   <b>Welcome:</b> Jerome Johnson II, Director of Professional Services –
                   U.S. Federal, Aerospace &amp; Defense, AWS
                 </td>
+                <td className="px-4 py-3">Blackburn Ballroom</td>
               </tr>
               <tr className="bg-gray-50">
-                <td className="px-4 py-3 whitespace-nowrap">9:40 AM – 9:50 AM</td>
+                <td className="px-4 py-3 whitespace-nowrap">9:40 AM – 10:00 AM</td>
                 <td className="px-4 py-3">
-                  <b>Opening Remarks:</b> Dr. Margie Vela, Strategic Academic Initiatives,
-                  Machine Learning University, Amazon Web Services
+                  <b><em>From 3D Printing to Soft Robotics & Drones: Ongoing Projects at Howard University</em></b>, Dr. Saurav Keshari Aryal, Senior Research Scientist, Institute for Human-Centered Artificial Intelligence, Howard University
+
+
                 </td>
+                <td className="px-4 py-3">Blackburn Ballroom</td>
               </tr>
               <tr>
-                <td className="px-4 py-3 whitespace-nowrap">9:50 AM – 10:20 AM</td>
+                <td className="px-4 py-3 whitespace-nowrap">10:00 AM – 10:20 AM</td>
                 <td className="px-4 py-3">
-                  Howard University Flash Talks 
+                  <b><em>Using Data from Diverse Sources to Understand the Health and Well-being of People and Communities</em></b>, Dr.  Anietie Andy, Assistant Professor, Department of Electrical Engineering and Computer Science, Howard University
                 </td>
+                <td className="px-4 py-3">Blackburn Ballroom</td>
               </tr>
               <tr className="bg-gray-50">
-                <td className="px-4 py-3 whitespace-nowrap">10:30 AM – 10:50 AM</td>
-                <td className="px-4 py-3">Plenary Keynote: TBD</td>
+                <td className="px-4 py-3 whitespace-nowrap">10:20 AM – 10:50 AM</td>
+                <td className="px-4 py-3">
+                  <b><em>Plenary Keynote: Charting a New Course: Our Journey toward Education Transformation</em></b>, Margie Vela, Strategic Academic Initiatives, Machine Learning University, Amazon Web Services
+                </td>
+                <td className="px-4 py-3">Blackburn Ballroom</td>
               </tr>
             </tbody>
           </table>
