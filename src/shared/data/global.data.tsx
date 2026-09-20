643054269457b10f2685c622f9883581207c85c5
elliejvela <ellie.j.vela@stanford.edu>
2026-09-20 09:55:49 -0700

    Self-host PDF

--- src/shared/data/global.data.tsx
+++ src/shared/data/global.data.tsx
@@ -12,7 +12,7 @@ import logoImg from "~/assets/images/MLU-full-logo-dark.png"
 export const studentAbstractSubmissionLink: string = "https://forms.cloud.microsoft/pages/responsepage.aspx?id=OpeucH3jSEmtxd4dP6row37VTpyqw1NEnO7K31VyHD9URDMzOUYwUlc0NUhXVFJYVjNYWjAyN0dDTS4u&route=shorturl";
 export const facultyAbstractSubmissionLink: string = "https://forms.cloud.microsoft/Pages/ResponsePage.aspx?id=OpeucH3jSEmtxd4dP6row37VTpyqw1NEnO7K31VyHD9UODQwN1gzRzEzTU9TWFFZWkNENDNTNDBDNC4u";
 export const symposiumRegistrationLink: string = 'https://app.brazenconnect.com/a/asp-sdengineering/e/9bRrV';
-export const knowBeforeYouGoLink: string = "https://drive.google.com/file/d/1wSyCoAPhtW-kGAkAGEMGRmr1LDP47_mr/view";
+export const knowBeforeYouGoLink: string = "2026_AWS_MLU_AI_Symposium_Know_Before_You_Go.pdf";
 export const howardTourGuideLink: string = "https://admission.howard.edu/sites/admission.howard.edu/files/2026-08/Self-Guided%20Tour%20Booklet.pdf"
 
 // Announcement data
